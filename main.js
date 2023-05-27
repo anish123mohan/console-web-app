8eec6c6e85f8d04cbdcbc24e5d594924f68ff148
Robotman <86520868+anish123mohan@users.noreply.github.com>
2023-05-27 11:39:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("My First Console Project");
+            var first_name="Anish ";
+            
+            var last_name="Mohan";
+            
+            console.log(first_name.concat(last_name));
+            
+            var a = 10;
+            
+            var b = 5;
+            
+            var add = a + b;
+            
+            console.log(add);
+            
+            
+            var subtract = a - b;
+            console.log(subtract);
+            
+            var multiply = a * b;
+            console.log(multiply);
+            
+            var divide = a / b;
+            console.log(divide);
